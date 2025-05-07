6526c6d298fb1691275e39c7e24d09414a5106e5
felipeembersisc <felipe.embersisc@gmail.com>
2025-05-07 13:05:21 -0300

    feat: finalizar a exportacao via csv

--- src/export.ts
+++ src/export.ts
@@ -1,4 +1,8 @@
+import { Transform } from 'node:stream'
+import { pipeline } from 'node:stream/promises'
 import { sql } from './db/client.ts'
+import { createWriteStream } from 'node:fs'
+import { stringify } from 'csv-stringify'
 
 const query = sql`
   SELECT id, name
@@ -8,9 +12,29 @@ const query = sql`
 
 const cursor = query.cursor(10)
 
-for await (const rows of cursor) {
-  console.log(rows)
-  break
-}
+const exampleStream = new Transform({
+  objectMode: true,
+  transform(chunk, encoding, callback) {
+    for (const item of chunk) {
+      this.push(item)
+    }
+
+    callback()
+  },
+})
+
+await pipeline(
+  cursor,
+  exampleStream,
+  stringify({
+    delimiter: ',',
+    header: true,
+    columns: [
+      { key: 'id', header: 'ID' },
+      { key: 'name', header: 'Name' }
+    ]
+  }),
+  createWriteStream('./export.csv', 'utf-8')
+)
 
 await sql.end()
